2c70f0d01113a3624aad433f0e9ee297f531a502
Gabriel Macaúbas <gabrielmacaubas@outlook.com>
2025-08-07 16:22:09 -0300

    feat: part 2

--- order/cmd/main.go
+++ order/cmd/main.go
@@ -1,18 +1,29 @@
+package main
+
 import (
 	"log"
-	"github.com/gabrielmacaubas/microservices/order/config "
+
+	"github.com/gabrielmacaubas/microservices/order/config"
 	"github.com/gabrielmacaubas/microservices/order/internal/adapters/db"
-	//"github.com/gabrielmacaubas/microservices/order/internal/adapters/rest"
+	payment_adapter "github.com/gabrielmacaubas/microservices/order/internal/adapters/payment"
+
 	"github.com/gabrielmacaubas/microservices/order/internal/adapters/grpc"
 	"github.com/gabrielmacaubas/microservices/order/internal/application/core/api"
 )
 
 func main() {
+
 	dbAdapter, err := db.NewAdapter(config.GetDataSourceURL())
 	if err != nil {
 		log.Fatalf(" Failed to connect to database . Error : %v", err)
 	}
-	application := api.NewApplication(dbAdapter)
+
+	paymentAdapter, err := payment_adapter.NewAdapter(config.GetPaymentServiceUrl())
+	if err != nil {
+		log.Fatalf(" Failed to initialize payment stub . Error : %v", err)
+	}
+
+	application := api.NewApplication(dbAdapter, paymentAdapter)
 	grpcAdapter := grpc.NewAdapter(application, config.GetApplicationPort())
 	grpcAdapter.Run()
 }
--- order/go.mod
+++ order/go.mod
@@ -4,3 +4,5 @@ go 1.24.3
 
 require github.com/gabrielmacaubas/microservices-proto/golang/order v0.0.0-00010101000000-000000000000
 replace github.com/gabrielmacaubas/microservices-proto/golang/order => ../../microservices-proto/golang/order
+require github.com/gabrielmacaubas/microservices-proto/golang/payment v0.0.0-00010101000000-000000000000
+replace github.com/gabrielmacaubas/microservices-proto/golang/payment => ../../microservices-proto/golang/payment
