aaa7f10834d691af976f069fa3f112f61d0dacdf
Gabriel Macaúbas <gabrielmacaubas@outlook.com>
2025-08-18 14:57:30 -0300

    feat: part four

--- order/internal/adapters/payment/payment.go
+++ order/internal/adapters/payment/payment.go
@@ -2,11 +2,16 @@ package payment_adapter
 
 import (
 	"context"
+	"log"
+	"time"
 
 	"github.com/gabrielmacaubas/microservices-proto/golang/payment"
 	"github.com/gabrielmacaubas/microservices/order/internal/application/core/domain"
+	grpc_retry "github.com/grpc-ecosystem/go-rpc-middleware/retry"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
 )
 
 type Adapter struct {
@@ -16,6 +21,14 @@ type Adapter struct {
 func NewAdapter(paymentServiceUrl string) (*Adapter, error) {
 	var opts []grpc.DialOption
 	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	opts = append(opts,
+		grpc.WithUnaryInterceptor(
+			grpc_retry.UnaryClientInterceptor(
+				grpc_retry.WithCodes(codes.Unavailable, codes.ResourceExhausted),
+				grpc_retry.WithMax(5),
+				grpc_retry.WithBackoff(grpc_retry.BackoffLinear(time.Second)),
+			)))
+	opts = append(opts, grpc.WithInsecure())
 	conn, err := grpc.Dial(paymentServiceUrl, opts...)
 	if err != nil {
 		return nil, err
@@ -24,10 +37,15 @@ func NewAdapter(paymentServiceUrl string) (*Adapter, error) {
 	return &Adapter{payment: client}, nil
 }
 func (a *Adapter) Charge(order *domain.Order) error {
-	_, err := a.payment.Create(context.Background(), &payment.CreatePaymentRequest{
+	ctx, _ := context.WithTimeout(context.Background(), 2*time.Second)
+	_, err := a.payment.Create(ctx, &payment.CreatePaymentRequest{
 		UserId:     order.CustomerID,
 		OrderId:    order.ID,
 		TotalPrice: order.TotalPrice(),
 	})
+
+	if status.Code(err) == codes.DeadlineExceeded {
+		log.Printf("Erro: deadline da requisição ao Payment estourou")
+	}
 	return err
 }
